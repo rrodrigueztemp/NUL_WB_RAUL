c2ed0aee115e7b556f47c00755327090f19ce7cf
Raul Rodriguez <rrodrigueztemp@nul.org>
2013-07-23 12:28:30 -0400

    work from home

---
+++ local.nul-wb.org/themes/custom/at_wb/templates/search-result.tpl.php
@@ -0,0 +1,25 @@
+<li class="search-result">
+  <h3 class="title"><a href="<?php print $url; ?>"><?php print $title; ?></a></h3>
+  <?php if ($snippet or $info_split): ?>
+    <div class="search-snippet-info">
+      <?php if ($snippet) : ?>
+        <p class="search-snippet"><?php print $snippet; ?></p>
+      <?php endif; ?>
+
+      <?php if ($info_split) : ?>
+        <p class="search-info">
+          <?php $separator = ''; ?>
+
+          <?php if (isset($info_split['date'])) : ?>
+            <span class="search-info-date"><?php print $separator . $info_split['date']; ?></span>
+            <?php $separator = $info_separator; ?>
+          <?php endif; ?>
+
+          <?php if (isset($info_split['upload'])) : ?>
+            <span class="search-info-upload"><?php print $separator . $info_split['upload']; ?></span>
+          <?php endif; ?>
+        </p>
+      <?php endif; ?>
+    </div>
+  <?php endif; ?>
+</li>
